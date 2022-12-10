8d643dc2d27535735128bb558a671aa29ddcf81f
A Stone <simplererstone@gmail.com>
2022-12-09 21:56:42 -0500

    all cleaned up and running nicely

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,6 @@ $(function () {
   var todaysDate = $('#currentDay');
   todaysDate.text(today);
   var currentHour = dayjs().hour();
-  var hour = ''
-  var comments
 
   $('.time-block').on('click', '.saveBtn', function(event) {
     ;
@@ -31,57 +29,16 @@ $(function () {
       } else {
         $(this).parent().addClass('future');
       }
-      // var savedTasks = localStorage.getItem($(this).parent().attr());
-      // $(this).val(savedTasks);
-      
+    
     });
 
-    
-    // var getTime = function () {
-    //   $('.description').each(function() {
-    //     var savedTasks = localStorage.getItem(hour);
-    //     var textArea = $(this).val()
-    //     textArea = savedTasks.val()
-    //     console.log(savedTasks)
-         
-    //   })
-    // }
-    
-         
     
   });
   
   
   
   
-  
-  
-  
-  
-  
-  // $('.row').each(function() {
-  //   })
-  // $(function () {
-  //   //Save button function
-  //   $(".saveBtn").on("click", function () {
-  //     var text = $(this).siblings(".col-md-9").val();
-  //     var time = $(this).parent().attr("id");
-  //     localStorage.setItem(time, text);
-  //   });
 
-  
- 
-
-//   $("#8 .col-md-9").val(localStorage.getItem("8"));
-//     $("#9 .col-md-9").val(localStorage.getItem("9"));
-//     $("#10 .col-md-9").val(localStorage.getItem("10"));
-//     $("#11 .col-md-9").val(localStorage.getItem("11"));
-//     $("#12 .col-md-9").val(localStorage.getItem("12"));
-//     $("#13 .col-md-9").val(localStorage.getItem("13"));
-//     $("#14 .col-md-9").val(localStorage.getItem("14"));
-//     $("#15 .col-md-9").val(localStorage.getItem("15"));
-//   });
-// });
 
 
 
