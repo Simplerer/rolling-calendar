e0ec139af79de5a65d036f109e8149438491fd8d
A Stone <simplererstone@gmail.com>
2022-12-09 21:54:03 -0500

    finally clicked, fixed refresh problem

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,22 @@ $(function () {
   var todaysDate = $('#currentDay');
   todaysDate.text(today);
   var currentHour = dayjs().hour();
+  var hour = ''
+  var comments
 
+  $('.time-block').on('click', '.saveBtn', function(event) {
+    ;
+    var comments = $(this).prev().val();
+    var hour = $(this).parent().attr('id');
+    
+    localStorage.setItem(hour, comments);
+    
+  })
+
+  $('.description').each(function() {
+    var savedTasks = localStorage.getItem($(this).parent().attr('id'));
+    $(this).val(savedTasks);
+  })
 
   $('.description').each(function() {
     var timeField = parseInt($(this).parent(0).attr('id'));
@@ -16,37 +31,57 @@ $(function () {
       } else {
         $(this).parent().addClass('future');
       }
+      // var savedTasks = localStorage.getItem($(this).parent().attr());
+      // $(this).val(savedTasks);
       
     });
 
-    // var reload = localStorage.getItem(hour, comments);
-    // console.log(reload);
-    // $('.time-block').each(functioin)() {
-    //   reload
+    
+    // var getTime = function () {
+    //   $('.description').each(function() {
+    //     var savedTasks = localStorage.getItem(hour);
+    //     var textArea = $(this).val()
+    //     textArea = savedTasks.val()
+    //     console.log(savedTasks)
+         
+    //   })
     // }
-
     
+         
     
-    $('.time-block').on('click', '.saveBtn', function(event) {
-          ;
-      var comments = $(this).prev().val();
-      var hour = $(this).parent().attr('id');
+  });
   
-      console.log(comments);
-      
   
-      localStorage.setItem(hour, comments);
-    })
-      
-    
-    
+  
+  
+  
+  
+  
+  
+  
+  // $('.row').each(function() {
+  //   })
+  // $(function () {
+  //   //Save button function
+  //   $(".saveBtn").on("click", function () {
+  //     var text = $(this).siblings(".col-md-9").val();
+  //     var time = $(this).parent().attr("id");
+  //     localStorage.setItem(time, text);
+  //   });
 
+  
  
 
-});
-
-
-
+//   $("#8 .col-md-9").val(localStorage.getItem("8"));
+//     $("#9 .col-md-9").val(localStorage.getItem("9"));
+//     $("#10 .col-md-9").val(localStorage.getItem("10"));
+//     $("#11 .col-md-9").val(localStorage.getItem("11"));
+//     $("#12 .col-md-9").val(localStorage.getItem("12"));
+//     $("#13 .col-md-9").val(localStorage.getItem("13"));
+//     $("#14 .col-md-9").val(localStorage.getItem("14"));
+//     $("#15 .col-md-9").val(localStorage.getItem("15"));
+//   });
+// });
 
 
 
